3c1abea146bdec93e401b4f7307ce7b2c73e56b5
Peteris Krumins <peteris.krumins@gmail.com>
2010-08-17 13:36:30 +0300

    update sync examples (gets rid of SyncImage and SyncStack)

--- tests/sync-stack.js
+++ tests/sync-stack.js
@@ -1,4 +1,4 @@
-var SyncStack = require('image').SyncStack;
+var Stack = require('image').Stack;
 var fs = require('fs');
 
 var desktop = fs.readFileSync('desktop.bgr');
@@ -8,9 +8,9 @@ var desktop = fs.readFileSync('desktop.bgr');
         console.log('jpeg is tricky, it needs .setBackground, passing it for now');
     }
     else {
-        var stack = new SyncStack(imageType, 'bgr');
+        var stack = new Stack(imageType, 'bgr');
         stack.push(desktop, 0, 0, 1024, 768);
-        var image = stack.encode();
+        var image = stack.encodeSync();
         fs.writeFileSync('desktop-stack-sync' + '.' + imageType, image, 'binary');
         var dims = stack.dimensions();
         console.log("x: %d, y: %d, w: %d, h: %d", dims.x, dims.y, dims.width, dims.height);
--- tests/sync.js
+++ tests/sync.js
@@ -1,11 +1,11 @@
-var SyncImage = require('image').SyncImage;
+var Image = require('image').Image;
 var fs = require('fs');
 
 var desktop = fs.readFileSync('desktop.bgr');
 
 ['png', 'gif', 'jpeg'].forEach(function (imageType) {
-    var image = new SyncImage(imageType, 'bgr');
+    var image = new Image(imageType, 'bgr');
     fs.writeFileSync('desktop-sync' + '.' + imageType,
-        image.encode(desktop, 1024, 768), 'binary');
+        image.encodeSync(desktop, 1024, 768), 'binary');
 });
 
